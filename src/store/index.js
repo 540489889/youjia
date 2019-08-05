372c65fa5f1df6ea86448e260a13630f453d9221
duanliang <540489889@qq.com>
2019-08-05 15:54:26 +0800

    order

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
-    AuthorizedUrl: 'http://192.168.137.1:8081/author/index',//授权url
+    AuthorizedUrl: 'http://yj.svkeji.cn/author/index',//授权url
     staticImg: '/static/img/',
     defaultTitle: '首页',
     IMGPATH: 'http://youjia.svkeji.cn',
